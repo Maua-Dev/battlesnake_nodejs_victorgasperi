a27ad9d362eb98574bcc595ec447b6b38f379c5a
Victor Gasperi <victorgasperi@gmail.com>
2024-07-26 20:50:35 -0300

    Test avoid walls 2

--- src/app/utils/avoid_walls.ts
+++ src/app/utils/avoid_walls.ts
@@ -7,8 +7,8 @@ export function avoid_walls(my_head: head, board_height: number, board_width: nu
     const futureMoves = possibleMovements(my_head);
 
     if(futureMoves["up"]["y"] === board_height) directions_to_avoid.push("up");
-    if(futureMoves["down"]["y"] > 0) directions_to_avoid.push("down");
-    if(futureMoves["left"]["x"] > 0) directions_to_avoid.push("left");
+    if(futureMoves["down"]["y"] < 0) directions_to_avoid.push("down");
+    if(futureMoves["left"]["x"] < 0) directions_to_avoid.push("left");
     if(futureMoves["right"]["x"] === board_width) directions_to_avoid.push("right");
 
     let aux = next_move.filter( direction => {
