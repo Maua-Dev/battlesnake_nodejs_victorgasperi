e439cfc5f7c4c8d13fe94b4e9f40feffefe0371d
Victor Gasperi <victorgasperi@gmail.com>
2024-07-29 19:07:37 -0300

    Add map board, needs the target implementation

---
+++ src/app/classes/mapGame.ts
@@ -0,0 +1,37 @@
+import { snake } from "../utils/info_interfaces";
+
+export class mapGame {
+  private gameBoard: number[][]; 
+
+  constructor(snakes: snake[]) {
+    this.gameBoard = this.mapCurrentBoard(snakes);
+  }
+
+  private mapCurrentBoard(snakes: snake[]): number[][] {
+    let gameBoard: number[][] = [
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]];
+    for (let i = 0; i < 11; i++) {
+      let snakesWithXCoodinate: snake[] = snakes.filter((snake) =>
+        snake.body.forEach((bodyCoordinate) => bodyCoordinate.x === i)
+      );
+      for (let j = 0; j < 11; j++) {
+        if (snakesWithXCoodinate === null) gameBoard[i][j] = 0;
+        snakesWithXCoodinate.forEach((snakeThatHasX) => {
+          snakeThatHasX.body.forEach((bodyCoordinate) => {
+            if (bodyCoordinate.y === j) gameBoard[i][j] = 1;
+          });
+        });
+      }
+    }
+    return gameBoard;
+  }
+}
